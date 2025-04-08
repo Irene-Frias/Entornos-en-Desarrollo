2d1a6c4aaaca15cced26d23b6ce63a3a5cc7c345
Irene-Frias-Ramos <ifriram1509@g.educaand.es>
2025-04-08 10:52:25 +0200

    Prueba control de versiones 2

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Estamos haciendo control de versiones "
 				+ "con Git");
+		System.out.println("Estamos probando nuevamente el control "
+				+ "de versiones de Git");
 	}
 
 }
